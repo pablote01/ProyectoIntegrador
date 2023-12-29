d1ea4581f6a8a8a28e67f37849e0e970ef554f5e
pablote01 <116689174+pablote01@users.noreply.github.com>
2023-12-29 01:23:27 -0300

    Add files via upload
    Subida del proyecto integrador

---
+++ app.js
@@ -0,0 +1,16 @@
+//Requires 
+
+const express = require ('express');
+const path = require('path')
+
+const app = express();
+const port = 3005
+
+//Config
+
+app.use (express.static(path.join(__dirname, 'public')))
+app.listen(port, () => (console.log('Nuestra App corre en http://localhost:'+port)))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views','home.html'))
+  });
